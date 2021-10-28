960c50b2c29a9de7d63db6af3885c97e2bcb75a2
BIGBANGATACK\FernandoJavier <fjcampano0592@gmail.com>
2021-10-28 20:33:03 -0300

    Commit

---
+++ Salvo/Salvo/Models/SalvoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Salvo.Models
+{
+    public class SalvoContext : DbContext
+    {
+        //constructor de la clase
+        public SalvoContext(DbContextOptions<SalvoContext> options) : base(options)
+        {
+
+        }
+    }
+}
